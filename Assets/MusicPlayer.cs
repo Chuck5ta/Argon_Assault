f4bdd31a8427f3543a7d28c723cfa88405f026cf
Chuck5ta <chuckstarrrr@googlemail.com>
2020-01-14 09:30:48 +0000

    80 - Setup A Splash Screen

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadScene", 2f); // load the scene after 2 seconds
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
