db4ad039aec9f9d9b5f34fbf9f1e23a03ef46c0d
Chuck5ta <chuckstarrrr@googlemail.com>
2020-01-20 07:52:16 +0000

    85 - Input Sensitivity & Gravity

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print("START");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // left and right movement (horizontal) across the screen of the ship
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        print(xOffsetThisFrame);
+    }
+}
